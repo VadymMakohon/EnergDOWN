c73d06a97c5918cfe57771d4b514250ab8376720
VadymMakohon <errorks7@gmail.com>
2024-10-16 13:50:11 +0200

    added model to the project

---
+++ model.js
@@ -0,0 +1,105 @@
+const model = {
+    // Del 1: 
+    app: {
+        currentPage: 'startPage',
+        // createAccountPage, loginPage, landingPage, loggedInUserProfile, userProfilePage, viewHistoryPage, addDrinkPage, addCustomDrinkPage, editGoalPage, editProfilePage, friendsListPage
+        loggedIn: false,
+    },
+
+    // Del 2: Inputs
+    inputs: {
+
+        startPage: {
+        },
+
+        createAccountPage: {
+            username: '',
+            email: '',
+            password: '',
+            image: '',
+        },
+
+
+        loginPage: {
+            username: '',
+            password: '',
+        },
+
+
+        landingPage: {
+        },
+
+
+        landingPageWithFriendList: {
+        },
+
+        loggedInUserProfile: {
+            sentComments: [],
+            favoriteDrinks: [],
+        },
+
+        userProfilePage: {
+            sentComments: [],
+            friendsList: [],
+        },
+
+        addDrinkPage: {
+            goalRegistrations: [
+                { goal: '', goalProgression: '', date: '', },
+            ],
+            favoriteDrinks: [],
+            drinksBeforeConfirmation: [],
+        },
+
+        editGoalPage: {
+            goalRegistrations: [
+                { goal: '', goalProgression: '', date: '', },
+            ],
+        },
+
+        friendsListPage: {
+            friendList: [],
+            searchFriend: '',
+        },
+
+        editProfilePage: {
+            username: '',
+            email: '',
+            password: '',
+            image: '',
+        },
+
+        viewHistoryPage: {
+            startDate: '',
+            endDate: '',
+        },
+
+        addCustomDrinkPage: {
+            drinkName: '',
+            coffeineContent: '',
+            drinkImage: '',
+            favoriteDrinks: [],
+        },
+
+    },
+
+    // Del 3: Felles data
+
+    registeredAccounts: [
+        {
+            id: 0,
+            username: '',
+            email: '',
+            password: '',
+            image: '',
+            goalRegistrations: [
+                { goal: '', goalProgression: '', date: '', },
+            ],
+            friendsList: [],
+            favoriteDrinks: [],
+            receivedComments: [],
+            sentComments: [],
+        }
+    ],
+
+}
