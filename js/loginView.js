2aeed57c9bd3f0b4efd789ae0c008a16670e4f4a
VadymMakohon <errorks7@gmail.com>
2024-10-21 12:58:16 +0200

    linked style to loginView.js

--- js/loginView.js
+++ js/loginView.js
@@ -2,7 +2,7 @@ function updateViewLogin() {
     document.getElementById('app').innerHTML = /*HTML*/`
         <header>
             <h1>Velkommen</h1>
-
+            <link rel="stylesheet" href="style.css">
             <div>
                 <div>Brukernavn:</div>
                 <input type = "text" oninput = "model.inputs.loginPage.username = this.value"/>
