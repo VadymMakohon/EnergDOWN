e7bc02dc9518b3651a4875947d88e4ddb5ed34a7
Rullekake <thommy926@gmail.com>
2024-10-21 12:42:31 +0200

    Update loginView.js

--- js/loginView.js
+++ js/loginView.js
@@ -1,5 +1,17 @@
 function updateViewLogin() {
     document.getElementById('app').innerHTML = /*HTML*/`
+        <header>
+            <h1>Velkommen</h1>
+
+            <div>
+                <div>Brukernavn:</div>
+                <input type = "text" oninput = "model.inputs.loginPage.username = this.value"/>
+                <div>Passord:</div>
+                <input type = "password" oninput = "model.inputs.loginPage.password = this.value"/>
+            </div>
+
+            <button onclick="logIn()">Logg inn</button>
     
+  </header>
     `;
 }
