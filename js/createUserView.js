562c126df826440fef2dde6522a57b3810890f5f
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-21 13:09:22 +0200

    opddatert createUserView og styles

--- js/createUserView.js
+++ js/createUserView.js
@@ -1,5 +1,26 @@
 function updateViewCreateUser() {
-    document.getElementById('app').innerHTML = /*HTML*/`
+  document.getElementById('app').innerHTML = /*HTML*/ `
+    
+    <div class="container-cuv">
+      <div class="left">
+        <input type="text" placeholder="Brukernavn:" required/>
+        <input type="email" placeholder="E-post:" required/>
+        <input type="password" placeholder="Passord" required/>
+      </div>
+      <div class="right">
+        <input
+          type="file"
+          id="fileInput"
+          accept="image/*"
+          style="display: none"
+        />
+        <label for="fileInput" class="image-label">
+          <img id="imagePreview" src="" alt="Egendefinert bilde" />
+        </label>
+      </div>
+    </div>
+    <button class="create-user">Opprett din bruker</button>
+    
     
     `;
 }
