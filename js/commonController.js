af10e356e830d07a34b553c8db01c3ed2f4f655c
Rullekake <thommy926@gmail.com>
2024-10-21 15:23:55 +0200

    Update commonController.js
    Laget en logOut() funksjon

--- js/commonController.js
+++ js/commonController.js
@@ -2,3 +2,10 @@ function goToPage(pageName) {
     model.app.currentPage = pageName;
     updateView();
 }
+
+function logOut() {
+    model.app.loggedInAsUserId = false;
+    model.inputs.loginPage.username = '';
+    model.inputs.loginPage.password = '';
+    goToPage('startPage');
+}
