41e6049db9e4b8c917381b9565d20c069836aa6c
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-21 13:39:29 +0200

    lagt til form element

--- js/createUserView.js
+++ js/createUserView.js
@@ -1,27 +1,26 @@
 function updateViewCreateUser() {
   document.getElementById('app').innerHTML = /*HTML*/ `
-    
-    <div class="container-cuv">
-    
-      <div class="left">
-        <input type="text" placeholder="Brukernavn:" required/>
-        <input type="email" placeholder="E-post:" required/>
-        <input type="password" placeholder="Passord" required/>
-      </div>
-      <div class="right">
-        <input
-          type="file"
-          id="fileInput"
-          accept="image/*"
-          style="display: none"
-        />
-        <label for="fileInput" class="image-label">
-          <span class="image-text">Egendefinert bilde</span>
-        </label>
-      </div>
-    </div>
-    <button class="create-user">Opprett din bruker</button>
-    
+    <form id="createUserForm">
+        <div class="container-cuv">
+        <div class="left">
+            <input type="text" placeholder="Brukernavn:" required/>
+            <input type="email" placeholder="E-post:" required/>
+            <input type="password" placeholder="Passord" required/>
+        </div>
+        <div class="right">
+            <input
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            style="display: none"
+            />
+            <label for="fileInput" class="image-label">
+            <span class="image-text">Egendefinert bilde</span>
+            </label>
+        </div>
+        </div>
+        <button class="create-user">Opprett din bruker</button>
+    </form>
     
     
     `;
