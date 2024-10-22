80c109ea9c428b57eb1bd77aa5510f227161f570
VadymMakohon <errorks7@gmail.com>
2024-10-22 10:43:38 +0200

    modified addDrinkView.js

--- js/addDrinkView.js
+++ js/addDrinkView.js
@@ -1,5 +1,20 @@
 function updateViewAddDrink() {
     document.getElementById('app').innerHTML = /*HTML*/`
-    
+    <div class="add-drink-view">
+        <button id="backBtn" class="back-btn">Tilbake</button>
+
+        <h3>Velg en drikke:</h3>
+        <ul id="drinkList" class="drink-list">
+            <li>Kaffe</li>
+            <li>Redbull</li>
+            <li>Battery</li>
+            <li>Monster</li>
+            <li>Energishot</li>
+        </ul>
+
+        <button id="customDrinkBtn" class="custom-drink-btn">Lag egendefinert drikke</button>
+
+        <button id="confirmBtn" class="confirm-btn">Bekreft at du har drukket dette</button>
+    </div>
     `;
 }
