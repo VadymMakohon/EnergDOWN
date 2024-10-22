b4233a3ed61c3fa7344a99bb37ddc731569abb68
Rullekake <thommy926@gmail.com>
2024-10-22 13:43:47 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ function createNavigationHtml() {
   let buttonsHtml = '';
   let friendsListButton = '';
   if (model.app.loggedIn == true) {
-    buttonsHtml = `<button class="profile-page-button" onclick="goToPage('userProfilePage')">Profil</button>
+    buttonsHtml = `<button class="profile-page-button" onclick="goToPage('loggedInUserProfilePage')">Profil</button>
                     <button class="loggut-button" onclick="logOut()">Logg ut</button>`;
     friendsListButton = `<button class="friends-button" onclick="logOut()">Venne liste</button>`;
   }
