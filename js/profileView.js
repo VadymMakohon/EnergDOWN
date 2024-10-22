2303e133f02e47123dc39843112fd86c36761d92
Christopher <185723871+christopherkp@users.noreply.github.com>
2024-10-22 13:36:04 +0200

    Update profileView.js

--- js/profileView.js
+++ js/profileView.js
@@ -4,15 +4,12 @@ function updateViewUserProfile() {
             <div class="userProfileSection" id="sectionUser">
                 <div id="username"><!-- getUsername !--></div>
                 <div id="profilePicture"><!-- getProfilePicture !--></div>
-                <button id="editProfileButton" onClick="goToPage('editProfilePage')">Endre profil</button>
-                <button id="friendsListButton" onClick="goToPage('friendsListPage')">Venneliste</button>
             </div>
             <div class="userProfileSection" id="userProfileSectionGoal">
                 <div class="userProfileTitle" id="userProfileTitleGoal">
                 <h1>Dagens mål</h1>
                 </div>
                 <div id="dailyGoalProgression"><!-- getGoalProgression !--></div>
-                <button id="editGoalButton" onClick="goToPage('editGoalPage')">Endre mål</button>
             </div>
             <div class="userProfileSection" id="userProfileSectionFavoriteDrinks">
                 <div class="userProfileTitle" id="userProfileTitleFavoriteDrinks">
@@ -25,10 +22,6 @@ function updateViewUserProfile() {
                     <h1>Kommentarer</h1>
                 </div>
                 <div id="comments"><!-- getUserProfileComments !--></div>
-            </div>
-            <div class="userProfileSection" id="userProfileSectionNavigation">
-                <button id="showHistoryButton" onClick="goToPage('viewHistoryPage')">Vis historikk</button>
-                <button id="addDrinkButton" onClick="goToPage('addDrinkPage')">Legg til drikke</button>
             </div>
         </div>
     `;
