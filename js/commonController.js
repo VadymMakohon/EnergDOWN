a9a36a972b62c33c38f455b7e41180ab397d6611
Rullekake <thommy926@gmail.com>
2024-10-22 14:39:32 +0200

    Flytte funksjon

--- js/commonController.js
+++ js/commonController.js
@@ -3,6 +3,30 @@ function goToPage(pageName) {
   updateView();
 }
 
+function allFriends() {
+  let userId = loggedInUserId();
+  let friends = [];
+  for (let otherUser of model.friends) {
+      if (otherUser.userId == userId ) 
+          friends.push(idToUser(otherUser.friendId));
+  }
+  return friends;
+}
+
+function loggedInUserId(){
+  for (let user of model.users) {
+      if (user.username == username) return user.id;
+  }
+  return -1;
+}
+
+function idToUser(id){
+for (let user of model.users) {
+  if (user.id == id) return user;
+}
+return null;
+}
+
 function logOut() {
   model.app.loggedIn = false;
   model.inputs.loginPage.username = '';
