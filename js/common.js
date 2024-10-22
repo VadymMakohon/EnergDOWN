eb3e0a5650a83ae0d366c359392d101abb8d43c8
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-22 14:40:59 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -17,3 +17,5 @@ function createNavigationHtml() {
         </div>
     `;
 }
+
+function showFriendList() {}
--- js/commonController.js
+++ js/commonController.js
@@ -7,24 +7,23 @@ function allFriends() {
   let userId = loggedInUserId();
   let friends = [];
   for (let otherUser of model.friends) {
-      if (otherUser.userId == userId ) 
-          friends.push(idToUser(otherUser.friendId));
+    if (otherUser.userId == userId) friends.push(idToUser(otherUser.friendId));
   }
   return friends;
 }
 
-function loggedInUserId(){
+function loggedInUserId() {
   for (let user of model.users) {
-      if (user.username == username) return user.id;
+    if (user.username == username) return user.id;
   }
   return -1;
 }
 
-function idToUser(id){
-for (let user of model.users) {
-  if (user.id == id) return user;
-}
-return null;
+function idToUser(id) {
+  for (let user of model.users) {
+    if (user.id == id) return user;
+  }
+  return null;
 }
 
 function logOut() {
@@ -33,5 +32,3 @@ function logOut() {
   model.inputs.loginPage.password = '';
   goToPage('startPage');
 }
-
-function showFriendList() {}
