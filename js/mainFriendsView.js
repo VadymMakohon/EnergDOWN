2156dd30149484cdccb594eb1167960cf695263a
Rullekake <thommy926@gmail.com>
2024-10-22 13:00:11 +0200

    La til navigasjon i main og mainFriends

--- js/mainFriendsView.js
+++ js/mainFriendsView.js
@@ -1,5 +1,26 @@
 function updateViewMainWithFriends() {
     document.getElementById('app').innerHTML = /*HTML*/`
-    
+    <button onclick="goToPage('loggedInUserProfilePage')">Profil</button>
+    <button onclick="logOut()">Logg ut</button>
+    <button onclick="goToPage('mainPage')">Venneliste</button>
+    <header>
+            
+            
+            <div>
+                Placeholder for getFriends();
+            </div>
+            <div>
+                Placeholder for getGoalGraph();
+            </div>
+            <div>
+                Placeholder for getSummery();
+            </div>
+            <div>
+                Placeholder for getMotivation();
+            </div>
+
+            <button onclick="goToPage('viewHistoryPage')">Vis historikk</button>
+            <button onclick="goToPage('addDrinkPage')">Legg til drikke</button>
+        </header>
     `;
 }
