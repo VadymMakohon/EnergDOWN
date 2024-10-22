a60f7c7f1b7375da5db5232973c445095a6366af
Christopher <185723871+christopherkp@users.noreply.github.com>
2024-10-22 13:35:56 +0200

    Update loggedInUserProfileView.js

--- js/loggedInUserProfileView.js
+++ js/loggedInUserProfileView.js
@@ -1,5 +1,35 @@
 function updateViewLoggedInUserProfile() {
     document.getElementById('app').innerHTML = /*HTML*/`
-    
+        <div id="userProfile"> 
+            <div class="userProfileSection" id="sectionUser">
+                <div id="username"><!-- getUsername !--></div>
+                <div id="profilePicture"><!-- getProfilePicture !--></div>
+                <button id="editProfileButton" onClick="goToPage('editProfilePage')">Endre profil</button>
+                <button id="friendsListButton" onClick="goToPage('friendsListPage')">Venneliste</button>
+            </div>
+            <div class="userProfileSection" id="userProfileSectionGoal">
+                <div class="userProfileTitle" id="userProfileTitleGoal">
+                <h1>Dagens mål</h1>
+                </div>
+                <div id="dailyGoalProgression"><!-- getGoalProgression !--></div>
+                <button id="editGoalButton" onClick="goToPage('editGoalPage')">Endre mål</button>
+            </div>
+            <div class="userProfileSection" id="userProfileSectionFavoriteDrinks">
+                <div class="userProfileTitle" id="userProfileTitleFavoriteDrinks">
+                    <h1>Favoritter</h1>
+                </div>
+                <div id="favoriteDrinks"><!-- getFavoriteDrinks !--></div>
+            </div>
+            <div class="userProfileSection" id="userProfileSectionComments">
+                <div class="userProfileTitle" id="userProfileTitleComments">
+                    <h1>Kommentarer</h1>
+                </div>
+                <div id="comments"><!-- getUserProfileComments !--></div>
+            </div>
+            <div class="userProfileSection" id="userProfileSectionNavigation">
+                <button id="showHistoryButton" onClick="goToPage('viewHistoryPage')">Vis historikk</button>
+                <button id="addDrinkButton" onClick="goToPage('addDrinkPage')">Legg til drikke</button>
+            </div>
+        </div>
     `;
 }
