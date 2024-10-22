1d38ca83464d6a5aed4bb7bf85cb551b1a01eee1
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-22 14:40:14 +0200

    Update loginView.js
    fix

--- js/loginView.js
+++ js/loginView.js
@@ -3,14 +3,14 @@ function updateViewLogin() {
     ${createNavigationHtml()}
         <header>
             <h1>Velkommen</h1>
-            
+            <form>
             <div class="login-container">
-                <input type = "text" placeholder="Brukernavn" oninput = "model.inputs.loginPage.username = this.value"/>
+                <input type = "text" placeholder="Brukernavn" required oninput = "model.inputs.loginPage.username = this.value"/>
               
-                <input type = "password" placeholder="Passord" oninput = "model.inputs.loginPage.password = this.value"/>
+                <input type = "password" placeholder="Passord" required oninput = "model.inputs.loginPage.password = this.value"/>
             </div>
-
             <button class="login-button" onclick="logIn()">Logg inn</button>
+            </form>
             <div style="color: red">${model.inputs.loginPage.error}</div>
         </header>
     `;
