2a8a16e9877927c34fa3c960cad1d9830ca99f88
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-22 13:30:43 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 function createNavigationHtml() {
   let buttonsHtml = '';
+  let friendsListButton = '';
   if (model.app.loggedIn == true) {
     buttonsHtml = `<button class="profile-page-button" onclick="goToPage('userProfilePage')">Profil</button>
-                    <button class="loggut-button" onclick="goToPage('startPage')">Logg ut</button>`;
+                    <button class="loggut-button" onclick="logOut()">Logg ut</button>`;
+    friendsListButton = `<button class="friends-button" onclick="logOut()">Venne liste</button>`;
   }
   return /*HTML*/ `
         <div class="navigation">
@@ -10,6 +12,8 @@ function createNavigationHtml() {
               ${buttonsHtml}
             </nav>
             <h2 class="name">EnergDOWN</h2>
+
+            ${friendsListButton}
         </div>
     `;
 }
--- js/commonController.js
+++ js/commonController.js
@@ -1,11 +1,13 @@
 function goToPage(pageName) {
-    model.app.currentPage = pageName;
-    updateView();
+  model.app.currentPage = pageName;
+  updateView();
 }
 
 function logOut() {
-    model.app.loggedInAsUserId = false;
-    model.inputs.loginPage.username = '';
-    model.inputs.loginPage.password = '';
-    goToPage('startPage');
+  model.app.loggedInAsUserId = false;
+  model.inputs.loginPage.username = '';
+  model.inputs.loginPage.password = '';
+  goToPage('startPage');
 }
+
+function showFriendList() {}
