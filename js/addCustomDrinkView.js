4d678998995caddd0cfbfef92d74b45c8589a12f
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-10-22 09:38:06 +0200

    completed add custom drink view

--- js/addCustomDrinkView.js
+++ js/addCustomDrinkView.js
@@ -1,5 +1,27 @@
 function updateViewAddCustomDrink() {
-    document.getElementById('app').innerHTML = /*HTML*/`
+  document.getElementById('app').innerHTML = /*HTML*/ `
+    <div class="container-acdv">
+        <div class="btn-div">
+            <button class="back-btn">Tilbake</button>
+        </div>
+        <div class="left">
+            <input type="text" placeholder="Navn:" required/>
+            <input type="number" placeholder="Koffeininnhold i mg:" required/>
+            
+        </div>
+        <div class="right">
+            <input
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            style="display: none"
+            />
+            <label for="fileInput" class="image-label-drink">
+            <span class="image-drink-text">Drikke bilde</span>
+            </label>
+        </div>
+        </div>
+        <button class="add-drink-btn">Legg til</button>
     
     `;
 }
