01a3f004c33aa4ed07c35163251c202ac7994f68
Christopher <185723871+christopherkp@users.noreply.github.com>
2024-10-24 11:20:33 +0200

    Update profileController.js

--- js/profileController.js
+++ js/profileController.js
@@ -12,11 +12,24 @@ function getProfilePicture(id) {
     }
 }
 
+function getFavoriteDrinks(id) {
+  let userProfileDrinks = [];
+  for (let favoriteDrinks of model.favoriteDrinks) {
+    if (favoriteDrinks.userId == id)
+      console.log(favoriteDrinks.drinkId);
+      for (let drinks of model.drinks) {
+        if (drinks.drinkId == favoriteDrinks.drinkId)
+        userProfileDrinks.push(drinks.name);
+      }
+  }
+  return userProfileDrinks;
+}
+
 function getComments(id) {
   let userProfileComments = [];
   for (let comments of model.comments) {
     if (comments.userId == id)
-        userProfileComments.push(comments.comment)
+        userProfileComments.push(comments.comment);
   }
   return userProfileComments;
 }
