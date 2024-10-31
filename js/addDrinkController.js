bd2d8fb293af04e24d0a68f6b4ba142d8a43d781
Rullekake <thommy926@gmail.com>
2024-10-31 13:29:34 +0100

    Update addDrinkController.js

--- js/addDrinkController.js
+++ js/addDrinkController.js
@@ -13,9 +13,18 @@ function drinksHTML() {
 
   if (drinks.length > 0) {
     for (const drink of drinks) {
-      let idOfSelectedDrink = drink.drinkId;
-      let drinkStyle = drink.selected ? 'gray' : 'white';
-      drinksHtml += `<li button style="background:${drinkStyle}" onclick="selectDrink(${idOfSelectedDrink})">${drink.name} ${drink.caffeineContent} mg ${favoriteStarHTML(loggedInUserId(), drink.drinkId)}`;
+      if (isDrinkFavorite(loggedInUserId(), drink.drinkId)) {
+        let idOfSelectedDrink = drink.drinkId;
+        let drinkStyle = drink.selected ? 'gray' : 'white';
+        drinksHtml += `<li button style="background:${drinkStyle}" onclick="selectDrink(${idOfSelectedDrink})">${drink.name} ${drink.caffeineContent} mg ${favoriteStarHTML(loggedInUserId(), drink.drinkId)}`;
+      }
+    }
+    for (const drink of drinks) {
+      if (!isDrinkFavorite(loggedInUserId(), drink.drinkId)) {
+        let idOfSelectedDrink = drink.drinkId;
+        let drinkStyle = drink.selected ? 'gray' : 'white';
+        drinksHtml += `<li button style="background:${drinkStyle}" onclick="selectDrink(${idOfSelectedDrink})">${drink.name} ${drink.caffeineContent} mg ${favoriteStarHTML(loggedInUserId(), drink.drinkId)}`;
+      }
     }
   }
   return drinksHtml;
