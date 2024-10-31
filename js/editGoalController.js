0c17c5f0b802765d60e5c5559ed2240369bec98a
Rullekake <thommy926@gmail.com>
2024-10-31 11:05:45 +0100

    Update editGoalController.js

--- js/editGoalController.js
+++ js/editGoalController.js
@@ -1,14 +1,29 @@
 function editGoal() {
     const newGoal = model.inputs.editGoalPage.goal;
     let id = loggedInUserId();
+    let counter = 0;
     if (loggedInUserId() < 0) return;
     
     for (let goal of model.goals){
         if(goal.userId == id){
+            counter++;
             goal.goal = newGoal;
         }
     }
-    
+    if(counter == 0){
+        createNewGoal(id,newGoal);
+    }
     goToPage('loggedInUserProfilePage');
+}
+
+function createNewGoal(id,goal){
+    let newGoal = {
+        id: 0,
+        goal: goal,
+        progression: 0,
+        date: '',
+        userId: id,
+      }
+      model.goals.push(newGoal);
 }
 
