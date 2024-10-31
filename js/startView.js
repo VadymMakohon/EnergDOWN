96feec9ef6ac987661894fa299f1a31a2fd53fcb
VadymMakohon <errorks7@gmail.com>
2024-10-31 13:34:47 +0100

    added style

--- js/startView.js
+++ js/startView.js
@@ -1,17 +1,17 @@
 function updateViewStart() {
   document.getElementById('app').innerHTML = /*HTML*/ `
-  ${createNavigationHtml()}
+    ${createNavigationHtml()}
     <header>
-    <h1>Velkommen</h1>
-    <p>
-      På denne nettsiden kan du holde oversikt over ditt koffeininntak. Du
-      kan sette deg daglige mål og motta motivasjon for å nå dem.
-    </p>
+      <h1>Velkommen</h1>
+      <p class="blinking-text">
+        På denne nettsiden kan du holde oversikt over ditt koffeininntak. Du
+        kan sette deg daglige mål og motta motivasjon for å nå dem.
+      </p>
 
-    <div class="button-container">
-      <button onclick="goToPage('loginPage')">Logg inn</button>
-      <button onclick="goToPage('createUserPage')">Opprett bruker</button>
-    </div>
-  </header>
-    `;
+      <div class="button-container">
+        <button onclick="goToPage('loginPage')">Logg inn</button>
+        <button onclick="goToPage('createUserPage')">Opprett bruker</button>
+      </div>
+    </header>
+  `;
 }
